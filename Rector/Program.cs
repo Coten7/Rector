2c3bba13c67a14340bc37fd4e062cca8ae75a54b
unicode <unicode@DESKTOP-Q1JCQRK>
2022-02-23 16:32:52 +0200

    Add project files.

---
+++ Rector/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Gateway;
+
+namespace Rector
+{
+    internal class Program
+    {
+
+        public static DiscordSocketClient clientd = new DiscordSocketClient();
+        static void Main(string[] args)
+        {
+            Connect();
+            Console.ReadKey();
+        }
+
+        private async static void Clientd_OnMessageReceived(DiscordSocketClient client, MessageEventArgs args)
+        {
+            await args.Message.AddReactionAsync("\uD83D\uDE00");
+        }
+
+        static async void Connect()
+        {
+            await Task.Run(() =>
+            {
+                clientd = new DiscordSocketClient(new DiscordSocketConfig() { RetryOnRateLimit = false });
+                clientd.Login("MzkwOTYzMTYzNzAyNzU1MzI4.YhY10g.5Crz6P78FS8jujqTpCd8FqR7HQI");
+            });
+            Console.WriteLine("Logged");
+            clientd.OnMessageReceived += Clientd_OnMessageReceived;
+        }
+    }
+}
